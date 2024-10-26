e05ee03d76d99ce5f8fd8759cebcd3e194d0cb33
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-10-26 18:37:43 -0500

    agregando cors a los servicios

---
+++ product-service/src/main/java/com/ecommerce/exception/ValidErrorResponse.java
@@ -0,0 +1,20 @@
+package com.ecommerce.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ValidErrorResponse {
+    private String message;
+    private Date TimeStamp;
+    private String Details;
+
+    public ValidErrorResponse(String message, Date timeStamp, String details) {
+        this.message = message;
+        this.TimeStamp = timeStamp;
+        this.Details = details;
+    }
+}
